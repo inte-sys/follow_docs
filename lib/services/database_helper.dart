8e32bee9e39afa7c2f3a3c5e61add63f9e68f045
Humberto Rincon <hubertpc@gmail.com>
2026-03-01 00:07:41 -0500

    feat: implement SQLite database helper with folder hierarchy support

---
+++ lib/services/database_helper.dart
@@ -0,0 +1,56 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/item_model.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('follow_docs.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    // Tabla para almacenar tanto carpetas como documentos [cite: 36]
+    await db.execute('''
+      CREATE TABLE items (
+        id TEXT PRIMARY KEY,
+        parent_id TEXT, 
+        name TEXT NOT NULL, 
+        type TEXT NOT NULL,
+        doc_type TEXT,
+        expiration_date TEXT,
+        reminder_type TEXT,
+        reminder_value INTEGER,
+        reminder_unit TEXT,
+        is_active INTEGER DEFAULT 1
+      )
+    ''');
+  }
+
+  Future<int> insertItem(Map<String, dynamic> row) async {
+    final db = await instance.database;
+    return await db.insert('items', row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllItems() async {
+    final db = await instance.database;
+    // Ordenar para que las carpetas aparezcan primero [cite: 51]
+    return await db.query('items', orderBy: "type ASC, name ASC");
+  }
+}
